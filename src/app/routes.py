2091c55613f9a26654b5f5e097ebd6bd4c7d3d70
nusaturn <skaturanus@proton.me>
2024-02-18 03:41:22 +0530

    quickfix 2:

--- src/app/routes.py
+++ src/app/routes.py
@@ -17,13 +17,13 @@ DATA_STORE_PATH = "data/user_doc"
 async def get_daily_map(
     goal: str,
 ):
-    response = await chains.arun_task_decomp(goal)
+    response = chains.run_task_decomp(goal)
     response = chains.llm_to_json(response["output"])
     return response
 
 
 @router.post("/roadmap/")
 async def search_similar_para(goal: str, background: str, expectations: str):
-    response = await chains.arun_roadgen(goal, background, expectations)
+    response = chains.run_roadgen(goal, background, expectations)
     response = chains.llm_to_json(response["output"])
     return response
